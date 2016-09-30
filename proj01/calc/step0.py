8b0ac478b40c2e2e019eaf845f615e1b013c0844
impurity <impurity@postech.ac.kr>
2016-09-30 21:36:13 +0900

    initial

--- proj01/calc/step0.py
+++ proj01/calc/step0.py
@@ -9,7 +9,14 @@ bulk = FaceCenteredCubic('Fe', latticeconstant = 3.6)
 
 calc = EMTO()
 
+os.system('export OMP_NUM_THREADS=1')
+os.system('export OMP_STACKSIZE=400m')
+os.system('mkdir work')
+os.chdir('work')
+
 bulk.set_calculator(calc)
 bulk.get_potential_energy()
+
+os.chdir('..')
 
 # view(bulk)
